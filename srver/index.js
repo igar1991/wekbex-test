c1b612db9715546a7c3c3a18b1465d78c44f52ba
igar1991 <iiggaarr19912004@gmail.com>
2022-10-21 18:40:41 +0300

    feat: add methods to server

--- srver/index.js
+++ srver/index.js
@@ -11,19 +11,54 @@ app.use(cors());
 app.use(express.json());
 
 const client = new MongoClient(MONGO_CONNECTION_STRING);
-
-app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));
-
-app.get("/city", async(req,res)=>{
+async function getPage(req, res, options) {
     try {
         await client.connect();
-        const cursor = await client.db("welbex").collection("city").find();
-        const results = await cursor.toArray();
-        return res.status(201).json(results)
+        const result = {};
+        const pageNumber = parseInt(req.query.page) || 0;
+        const limit = parseInt(req.query.limit) || 12;
+        let startIndex = pageNumber * limit;
+        const endIndex = (pageNumber + 1) * limit;
+        const count = await (await client.db("welbex").collection("welbex").find(options).toArray()).length;
+        result.totalCities = count;
+        if (startIndex > 0) {
+          result.previous = {
+            pageNumber: pageNumber - 1,
+            limit: limit,
+          };
+        }
+        if (endIndex < count) {
+          result.next = {
+            pageNumber: pageNumber + 1,
+            limit: limit,
+          };
+        }
+        result.data = await client.db("welbex").collection("welbex").find(options)
+        .sort({"title": 1})
+        .skip(startIndex)
+        .limit(limit)
+        .toArray()
+        result.rowsPerPage = limit;
+        return res.json({ msg: "Post Fetched successfully", data: result });
     } catch (e) {
         console.error(e);
-        return res.sendStatus(500);
+        return res.status(500);
     } finally {
         await client.close();
     }
+}
+
+app.get("/api/city", async(req,res)=>{
+    await getPage(req,res, {})
 });
+
+app.post("/api/city", async(req,res)=>{
+    const options = {};
+    options[req.body.column] = {[req.body.condition]: req.body.column==='title'?req.body.value:Number(req.body.value)}
+    if(options['title']){
+        options[req.body.column].$options = "i"
+    }
+    await getPage(req,res, options)
+});
+
+app.listen(PORT, () => console.log(`App is running on port: ${PORT}`));	
